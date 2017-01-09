7a8258beed7773526c73f5f66dd0dec694db7ef2
Nox <shadowsirius@gmail.com>
2017-01-09 23:34:01 +0300

    Base

---
+++ src/com/nox/classes/interruptAction.java
@@ -0,0 +1,25 @@
+package com.nox.classes;
+
+/**
+ * Created by Nox on 09.01.2017.
+ */
+public class interruptAction {
+    private int initiativePrice;
+    private String interruptName;
+
+    public int getInitiativePrice() {
+        return initiativePrice;
+    }
+
+    public void setInitiativePrice(int initiativePrice) {
+        this.initiativePrice = initiativePrice;
+    }
+
+    public String getInterruptName() {
+        return interruptName;
+    }
+
+    public void setInterruptName(String interruptName) {
+        this.interruptName = interruptName;
+    }
+}
---
+++ src/com/nox/classes/person.java
@@ -0,0 +1,61 @@
+package com.nox.classes;
+
+/**
+ * Created by Nox on 09.01.2017.
+ */
+public class person {
+    private int reaction;
+    private int intuition;
+    private int initiativeDice;
+    private int initiativeCount;
+    private String name;
+    private String player;
+
+    public int getReaction() {
+        return reaction;
+    }
+
+    public void setReaction(int reaction) {
+        this.reaction = reaction;
+    }
+
+    public int getIntuition() {
+        return intuition;
+    }
+
+    public void setIntuition(int intuition) {
+        this.intuition = intuition;
+    }
+
+    public int getInitiativeDice() {
+        return initiativeDice;
+    }
+
+    public void setInitiativeDice(int initiativeDice) {
+        this.initiativeDice = initiativeDice;
+    }
+
+    public int getInitiativeCount() {
+        return initiativeCount;
+    }
+
+    public void setInitiativeCount(int initiativeCount) {
+        this.initiativeCount = initiativeCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+}
