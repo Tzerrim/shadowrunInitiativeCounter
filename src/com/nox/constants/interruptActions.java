1344626250a0e4589d0e92d956e5823902e7ad26
Nox <shadowsirius@gmail.com>
2017-01-12 06:35:56 +0300

    interrupt actions. Add to constants word "final"

--- src/com/nox/constants/interruptActions.java
+++ src/com/nox/constants/interruptActions.java
@@ -7,21 +7,21 @@ import com.nox.classes.interruptAction;
  */
 public class interruptActions {
     // Frome Core book
-    public static interruptAction fullDefence =  new interruptAction(10,"Full defence");
-    public static interruptAction block = new interruptAction( 5, "Block");
-    public static interruptAction dodge = new interruptAction( 5 , "Dodge");
-    public static interruptAction layDown = new interruptAction( 5, "Lay down");
-    public static interruptAction intercept = new interruptAction(5, "Intercept");
-    public static interruptAction parry = new interruptAction(5, "Parry");
+    public static final interruptAction fullDefence =  new interruptAction(10,"Full defence");
+    public static final interruptAction block = new interruptAction( 5, "Block");
+    public static final interruptAction dodge = new interruptAction( 5 , "Dodge");
+    public static final interruptAction layDown = new interruptAction( 5, "Lay down");
+    public static final interruptAction intercept = new interruptAction(5, "Intercept");
+    public static final interruptAction parry = new interruptAction(5, "Parry");
     // From Run&Gun
-    public static interruptAction countestrike = new interruptAction(7, "Counterstrike");
-    public static interruptAction diveForCover = new interruptAction(5,"Dive for cover");
-    public static interruptAction reversal = new interruptAction(7,"Reversal");
-    public static interruptAction rightBackAtYa = new interruptAction(10, "Right back at ya!");
-    public static interruptAction runForYourLife = new interruptAction(5, "Run for your life");
-    public static interruptAction diveOnTheGrenade = new interruptAction(5, "Dive on the grenade");
-    public static interruptAction sacrificeThrow = new interruptAction(10, "Sacrifice throw");
-    public static interruptAction riposte = new interruptAction(7, "Riposte");
-    public static interruptAction protectingThePrinciple = new interruptAction(5, "Protecting the principle");
-    public static interruptAction shadowBlock = new interruptAction(5,"Shadow block");
+    public static final interruptAction countestrike = new interruptAction(7, "Counterstrike");
+    public static final interruptAction diveForCover = new interruptAction(5,"Dive for cover");
+    public static final interruptAction reversal = new interruptAction(7,"Reversal");
+    public static final interruptAction rightBackAtYa = new interruptAction(10, "Right back at ya!");
+    public static final interruptAction runForYourLife = new interruptAction(5, "Run for your life");
+    public static final interruptAction diveOnTheGrenade = new interruptAction(5, "Dive on the grenade");
+    public static final interruptAction sacrificeThrow = new interruptAction(10, "Sacrifice throw");
+    public static final interruptAction riposte = new interruptAction(7, "Riposte");
+    public static final interruptAction protectingThePrinciple = new interruptAction(5, "Protecting the principle");
+    public static final interruptAction shadowBlock = new interruptAction(5,"Shadow block");
 }
