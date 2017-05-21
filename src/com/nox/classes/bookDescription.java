88fec95466176a997e6451b18f0298330d8d6229
Tzerrim <Me4z9Uirh42LteB66s>
2017-05-21 23:40:37 +0300

    Preparing for add description text from books

---
+++ src/com/nox/classes/bookDescription.java
@@ -0,0 +1,10 @@
+package com.nox.classes;
+
+/**
+ * Created by Nox on 21.05.2017.
+ */
+public class bookDescription {
+    private String source;
+    private String text;
+    private String initiativeCost;
+}
---
+++ src/com/nox/constants/bookDescriptions.java
@@ -0,0 +1,7 @@
+package com.nox.constants;
+
+/**
+ * Created by Nox on 21.05.2017.
+ */
+public class bookDescriptions {
+}
