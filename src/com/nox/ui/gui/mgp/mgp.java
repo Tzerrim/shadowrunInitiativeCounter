55a419a84528c118dbff0dd425f012a4192f37cc
Nox <shadowsirius@gmail.com>
2017-08-15 21:07:25 +0300

    Next step

---
+++ src/com/nox/ui/gui/mgp/mgp.java
@@ -0,0 +1,11 @@
+package com.nox.ui.gui.mgp;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class mgp {
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+    }
+}
