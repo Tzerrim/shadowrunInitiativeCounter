3ccaf98742835a180c091799877941b0047da1d7
Nox <shadowsirius@gmail.com>
2017-10-04 22:01:19 +0300

    Next step

--- src/com/nox/ui/gui/mgp/mgp.java
+++ src/com/nox/ui/gui/mgp/mgp.java
@@ -1,11 +1,30 @@
 package com.nox.ui.gui.mgp;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 
 public class mgp {
-    public Label helloWorld;
+    @FXML    private MenuBar mainMenu;
+    @FXML    private TableView tableView;
+    @FXML    private MenuBar lowMenu;
+    @FXML    private TextArea textArea;
 
-    public void sayHelloWorld(ActionEvent actionEvent) {
+    public void initialize(){
+        textArea.setText("Test text");
     }
+
+    @FXML
+    private void handleCloseAction(ActionEvent event) {
+        System.out.println("exiting");
+        System.exit(0);
+        Platform.exit();
+    }
+
+
+
 }
