f3d257c3b803218917284717e389f6cadc970c41
Nox <shadowsirius@gmail.com>
2017-10-04 21:31:10 +0300

    Next step

--- src/com/nox/constants/interruptActions.java
+++ src/com/nox/constants/interruptActions.java
@@ -10,42 +10,42 @@ import java.util.Arrays;
  */
 public class interruptActions {
     // From Core book
-    public static final interruptAction fullDefence =  new interruptAction(10,"Full defence");
-    public static final interruptAction block = new interruptAction( 5, "Block");
-    public static final interruptAction dodge = new interruptAction( 5 , "Dodge");
-    public static final interruptAction layDown = new interruptAction( 5, "Lay down");
-    public static final interruptAction intercept = new interruptAction(5, "Intercept");
-    public static final interruptAction parry = new interruptAction(5, "Parry");
+    public static final interruptAction FULL_DEFENCE =  new interruptAction(10,"Full defence");
+    public static final interruptAction BLOCK = new interruptAction( 5, "Block");
+    public static final interruptAction DODGE = new interruptAction( 5 , "Dodge");
+    public static final interruptAction LAY_DOWN = new interruptAction( 5, "Lay down");
+    public static final interruptAction INTERCEPT = new interruptAction(5, "Intercept");
+    public static final interruptAction PARRY = new interruptAction(5, "Parry");
     // From Run&Gun
-    public static final interruptAction countestrike = new interruptAction(7, "Counterstrike");
-    public static final interruptAction diveForCover = new interruptAction(5,"Dive for cover");
-    public static final interruptAction reversal = new interruptAction(7,"Reversal");
-    public static final interruptAction rightBackAtYa = new interruptAction(10, "Right back at ya!");
-    public static final interruptAction runForYourLife = new interruptAction(5, "Run for your life");
-    public static final interruptAction diveOnTheGrenade = new interruptAction(5, "Dive on the grenade");
-    public static final interruptAction sacrificeThrow = new interruptAction(10, "Sacrifice throw");
-    public static final interruptAction riposte = new interruptAction(7, "Riposte");
-    public static final interruptAction protectingThePrinciple = new interruptAction(5, "Protecting the principle");
-    public static final interruptAction shadowBlock = new interruptAction(5,"Shadow block");
+    public static final interruptAction COUNTERSTRIKE = new interruptAction(7, "Counterstrike");
+    public static final interruptAction DIVE_FOR_COVER = new interruptAction(5,"Dive for cover");
+    public static final interruptAction REVERSAL = new interruptAction(7,"Reversal");
+    public static final interruptAction REGHIT_BACK_AT_YA = new interruptAction(10, "Right back at ya!");
+    public static final interruptAction RUN_FOR_YOUR_LIFE = new interruptAction(5, "Run for your life");
+    public static final interruptAction DIVE_ON_THE_GRENADE = new interruptAction(5, "Dive on the grenade");
+    public static final interruptAction SACRIFICE_THROW = new interruptAction(10, "Sacrifice throw");
+    public static final interruptAction RIPOSTE = new interruptAction(7, "Riposte");
+    public static final interruptAction PROTECTING_THE_PRINCIPLE = new interruptAction(5, "Protecting the principle");
+    public static final interruptAction SHADOW_BLOCK = new interruptAction(5,"Shadow block");
 
     public static final ArrayList<interruptAction> interruptList = new ArrayList<>(
             Arrays.asList(
-                    fullDefence,
-                    block,
-                    dodge,
-                    layDown,
-                    intercept,
-                    parry,
-                    countestrike,
-                    diveForCover,
-                    reversal,
-                    rightBackAtYa,
-                    runForYourLife,
-                    diveOnTheGrenade,
-                    sacrificeThrow,
-                    riposte,
-                    protectingThePrinciple,
-                    shadowBlock
+                    FULL_DEFENCE,
+                    BLOCK,
+                    DODGE,
+                    LAY_DOWN,
+                    INTERCEPT,
+                    PARRY,
+                    COUNTERSTRIKE,
+                    DIVE_FOR_COVER,
+                    REVERSAL,
+                    REGHIT_BACK_AT_YA,
+                    RUN_FOR_YOUR_LIFE,
+                    DIVE_ON_THE_GRENADE,
+                    SACRIFICE_THROW,
+                    RIPOSTE,
+                    PROTECTING_THE_PRINCIPLE,
+                    SHADOW_BLOCK
             ));
 
 }
--- src/com/nox/constants/strings.java
+++ src/com/nox/constants/strings.java
@@ -4,16 +4,16 @@ package com.nox.constants;
  * Created by Nox on 18.01.2017.
  */
 public class strings {
-    public static final String[] columnNames = {"Pers. name", "Initiative",  "Combat turn"};
-    public static final String action = "ACTION";
-    public static final String interruptAction = "INTERRUPT ACTION";
-    public static final String initiaitveRun = "INITIATIVE RUN";
-    public static final String initiativeTurn = "INITIATIVE TURN";
+    public static final String[] COLUMN_NAMES = {"Pers. name", "Initiative",  "Combat turn"};
+    public static final String ACTION = "ACTION";
+    public static final String INTERRUPT_ACTION = "INTERRUPT ACTION";
+    public static final String INITIATIVE_RUN = "INITIATIVE RUN";
+    public static final String INITIATIVE_TURN = "INITIATIVE TURN";
     // Buttons
-    public static final String newBattle = "New battle";
-    public static final String safeLog = "Safe log";
-    public static final String endBattle = "End battle";
-    public static final String passTurn = "Pass turn";
-    public static final String endTurn = "End turn";
+    public static final String NEW_BATTLE = "New battle";
+    public static final String SAFE_LOG = "Safe log";
+    public static final String END_BATTLE = "End battle";
+    public static final String PASS_TURN = "Pass turn";
+    public static final String END_TURN = "End turn";
 
 }
