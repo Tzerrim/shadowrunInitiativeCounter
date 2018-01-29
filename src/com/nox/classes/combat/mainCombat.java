6652b0f3747240ed600761dbdd2553ba70d8f47e
Tzerrim <Me4z9Uirh42LteB66s>
2018-01-29 18:32:45 +0300

    combat MAIN PART

--- src/com/nox/classes/combat/mainCombat.java
+++ src/com/nox/classes/combat/mainCombat.java
@@ -1,6 +1,10 @@
 package com.nox.classes.combat;
 
+import com.nox.classes.factories.simpleCombatTurnFactory;
+import com.nox.interfaces.action;
 import com.nox.interfaces.combat;
+import com.nox.interfaces.combatResponse;
+import com.nox.interfaces.personage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +13,27 @@ public class mainCombat extends combat {
     private combatTurn currentCombatTurn;
     private ArrayList<combatTurn> combatTurns;
 
+    public mainCombat(ArrayList<personage> personages){
+        this.personages = personages;
+        this.count = 0;
+        this.currentCombatTurn = (combatTurn) simpleCombatTurnFactory.getInstance().create(0, personages);
+        combatTurns = new ArrayList<combatTurn>();
+    }
 
+    public combatResponse doCombat(action action){
+        personage pers = personages.get(0);
+        combatResponse =  currentCombatTurn.doCombat(action);
+        if(combatResponse.isMakeNew()) {
+            combatTurns.add(currentCombatTurn);
+            currentCombatTurn = (combatTurn) simpleCombatTurnFactory.getInstance().create(combatTurns.size() -1, personages);
+            combatResponse =  currentCombatTurn.doCombat(action);
+        }
+
+        return combatResponse;
+    }
+
+    protected boolean checkPersonage () {
+        return true;
+    }
 
 }
